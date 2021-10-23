66dce7b84e3edb4f9af34284ce59dce433b6bd2b
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2021-10-23 13:30:17 +0530

    Create Abhishek.java

---
+++ Abhishek.java
@@ -0,0 +1,13 @@
+public class Abhishek {
+    public static void main(String args[]){
+        int i, j, n=10;
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<=i; j++)
+            {
+                System.out.print((j%2)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
